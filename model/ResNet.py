e7901ee7a3fa9603c4880dd469a94e8fa27cbc05
杨邵军 <ccnuysj@qq.com>
2020-03-09 18:38:26 +0800

    vgg and resnet

---
+++ model/ResNet.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from torch.utils.data import dataloader, dataset
+from torchvision import models
+from torchsummary import summary
+
+
+class ResBlock(nn.Module):
+    def __init__(self, input_channel, out_channel, stride=1):
+        super(ResBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(input_channel, out_channel, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channel)
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or input_channel != out_channel:
+            # 输入输出的维度不同，进行下采样
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(input_channel, out_channel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channel)
+            )
+
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = self.relu2(out)
+        return out
+
+
+class BottleNeck(nn.Module):
+    def __init__(self, input_channel, out_channel, stride=1):
+        super(BottleNeck, self).__init__()
+        self.down_sample = int(out_channel / 4)
+
+        print(self.down_sample,input_channel,out_channel)
+        self.left = nn.Sequential(
+            nn.Conv2d(input_channel, self.down_sample, 1, stride=1,bias=False),
+            nn.BatchNorm2d(self.down_sample),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.down_sample, self.down_sample, 3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(self.down_sample),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.down_sample, out_channel, 1, stride=1,bias=False),
+        )
+        self.shortcut = nn.Sequential()
+        if input_channel != out_channel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(input_channel, out_channel, 1, stride=stride, bias=True),
+                nn.BatchNorm2d(out_channel)
+            )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_layers, block, num_classes=10):
+        self.input_channel = 64
+        super(ResNet, self).__init__()
+        self.downsample = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.res = nn.Sequential(
+            self.layer(block, 64, num_layers[0], 2),
+            self.layer(block, 128, num_layers[1], 2),
+            self.layer(block, 256, num_layers[2], 2),
+            self.layer(block, 512, num_layers[3], 2),
+            nn.AvgPool2d(2),
+            nn.Flatten(),
+            nn.Linear(512,num_classes)
+        )
+
+    def layer(self, block, out_channel, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        res_layer = []
+        for stride in strides:
+            res_layer.append(block(self.input_channel, out_channel, stride))
+            self.input_channel = out_channel
+        return nn.Sequential(*res_layer)
+
+    def forward(self, x):
+        out = self.downsample(x)
+        out = self.res(out)
+        return out
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+net = ResNet([3,4,6,3],ResBlock, 10).to(device)
+summary(net, (3, 32, 32))
---
+++ model/VGG.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+import torch.optim as optim
+import numpy as np
+from torchvision import datasets, transforms, models
+
+
+def panel3X3(input_channel, out_channel):
+    return nn.Sequential(
+        nn.Conv2d(input_channel, out_channel, 3, stride=1, padding=1, bias=True),
+        nn.BatchNorm2d(out_channel),
+        nn.ReLU(inplace=True),
+    )
+
+
+def panel1X1(input_channel, out_channel):
+    return nn.Sequential(
+        nn.Conv2d(input_channel, out_channel, 1, stride=1, padding=1, bias=True),
+        nn.BatchNorm2d(out_channel),
+        nn.ReLU(inplace=True),
+    )
+
+classafication = nn.Sequential(
+    *[nn.Flatten(), nn.Linear(512, 512), nn.Linear(512, 512), nn.Linear(512, 10)]
+)
+
+class VGG11(nn.Module):
+    def __init__(self,lrn=False):
+        super(VGG11, self).__init__()
+
+        self.downsample = nn.Sequential(
+            *[panel3X3(3,64) ,nn.LocalResponseNorm(64) if lrn else nn.Sequential()],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(64, 128)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(128, 256),panel3X3(256, 256)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(256, 512),panel3X3(512, 512)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(512, 512),panel3X3(512, 512)],
+            nn.MaxPool2d(2, 2),
+
+            classafication
+        )
+
+    def forward(self, x):
+        out = self.downsample(x)
+        return out
+
+
+class VGG16(nn.Module):
+    def __init__(self,size=3):
+        super(VGG16,self).__init__()
+
+        self.downsample = nn.Sequential(
+            *[panel3X3(3, 64), panel3X3(64,64)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(64, 128)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(128, 256), panel3X3(256, 256),panel3X3(256, 256) if size==3 else panel1X1(356,256)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(256, 512), panel3X3(512, 512),panel3X3(512, 512) if size==3 else panel1X1(356,256)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(512, 512), panel3X3(512, 512),panel3X3(512, 512) if size==3 else panel1X1(356,256)],
+            nn.MaxPool2d(2, 2),
+
+            classafication
+        )
+
+    def forward(self,x):
+        out = self.downsample(x)
+        return out
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19,self).__init__()
+
+        self.downsample = nn.Sequential(
+            *[panel3X3(3, 64), panel3X3(64,64)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(64, 128)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(128, 256), panel3X3(256, 256),panel3X3(256, 256),panel3X3(256, 256)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(256, 512), panel3X3(512, 512),panel3X3(512, 512),panel3X3(512, 512)],
+            nn.MaxPool2d(2, 2),
+
+            *[panel3X3(512, 512), panel3X3(512, 512),panel3X3(512, 512),panel3X3(512, 512)],
+            nn.MaxPool2d(2, 2),
+
+            classafication
+        )
+
+    def forward(self,x):
+        out = self.downsample(x)
+        return out
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = VGG11(lrn=True).to(device)
+summary(model,(3,32,32))
