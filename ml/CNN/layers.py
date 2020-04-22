9be21264fcaa74e7810dfb28bc4a1ac49aea507d
杨邵军 <ccnuysj@qq.com>
2020-04-22 22:48:11 +0800

    softmax back

--- ml/CNN/layers.py
+++ ml/CNN/layers.py
@@ -42,6 +42,8 @@ class Conv(object):
         mini_batch,output_channel,w,h = y.shape
         if self.padding != 0:
             x = np.pad(self.input_maps, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), 'constant')
+        else:
+            x= self.input_maps
         for idc in range(len(self.core)):
             #更新一个核,core 3*3*3
             core = self.core[idc,:,:,:]
@@ -58,12 +60,14 @@ class Conv(object):
                             for seq in range(self.input_channel):
                                 deltacore[seq,i,j] += np.mean(x[idf,seq,i:i + w, j:j + h] * feature)
             deltacore=deltacore/len(y)
-            self.core[idc,:,:,:]+=deltacore*3
+            self.core[idc,:,:,:]+=deltacore*0.3
 
     def backword(self,y):
         # 输入为 output_maps，更新core的
         # 输入为 128 * 10 * 10 *10
         # 计算输入点产生的误差
+        t = time.time()
+
         self.update(y)
         mini_batch, c, w, h = y.shape
         mini_batch, ic, iw, ih = self.input_maps.shape
@@ -86,6 +90,8 @@ class Conv(object):
                     # 得到特征图加到误差点上
                 self.gradient[idx,:,:,:]+=featuremap
         self.output_maps =[]
+
+        # print("conv back",time.time()-t)
         #输出大小为input_maps,去掉padding
 
 class activition(object):
@@ -102,7 +108,9 @@ class activition(object):
         x[x > 0] = 1
         return x
     def backword(self,y):
+        t = time.time()
         self.gradient = self.prime(y)
+        # print ("activition",time.time()-t)
 
 
 
@@ -158,12 +166,23 @@ class MaxPooling(object):
         return x,y,val
 
     def backword(self,y):
+        t = time.time()
+        # print(" maxpooling")
         #input 尺寸为 10 * 5 * 5,recordmap尺寸为128 *10*5*5
         y = np.repeat(np.repeat(y,self.stride,axis=2),self.stride,axis=3)
+        # print("maxpooling back",time.time()-t)
+
         resshape = np.array(self.input_maps.shape) - np.array(y.shape)
+        # print("maxpooling back",time.time()-t)
+
         y = np.pad(y,((0,resshape[0]),(0,resshape[1]),(0,resshape[2]),(0,resshape[3])),'constant')
+        # print("maxpooling back",time.time()-t)
+
         self.gradient = y*self.record_maps
+        # print("maxpooling back",time.time()-t)
+
         self.output_maps = []
+        # print("maxpooling back",time.time()-t)
 
 
 class Flattan(object):
@@ -177,16 +196,18 @@ class Flattan(object):
         for maps in x:
             self.output_maps.append(maps.flatten())
         self.output_maps = np.array(self.output_maps)
+        print (self.output_maps.shape)
         #输出是 128 * 250
     def backword(self,y):
-        print("flattan-back",y.shape)
+        # t = time.time()
         self.gradient = y.reshape(self.input_maps.shape)
         self.output_maps = []
+        # print("flattan back",time.time()-t)
+
 
 
 class FC(object):
     def __init__(self,input_nums,output_nums,bias=False):
-        self.biases = np.random.randn((1,output_nums)) if bias else np.zeros((1,output_nums))
         self.weights = np.random.randn(input_nums, output_nums) # 250 * 10
         self.output_maps =None
         self.gradient =None
@@ -198,13 +219,14 @@ class FC(object):
 
     def update(self,y):
         # input 128 *250 y 128 * 10
-        # print(np.dot(self.input_maps.T,y))
-        self.weights+=0.3*np.dot(self.input_maps.T,y)
+        self.weights+=0.3*np.dot(self.input_maps.T,y)/10
 
     def backword(self,y):
         # y 128 * 30
+        t = time.time()
         self.update(y)
         self.gradient = np.dot(y,self.weights.T)
+        # print("fc back",time.time()-t)
 
 class softMax(object):
     def __init__(self):
@@ -214,12 +236,17 @@ class softMax(object):
     def forword(self,x):
         # 输入为 128 * 10
         self.input_maps = x
-        sum = np.sum(np.exp(x),axis=1).reshape((2,1))
+        mini_batch,num_class = x.shape
+        print (x)
+        sum = np.sum(np.exp(x),axis=1).reshape((mini_batch,1))
         self.output_maps = np.exp(x)/sum
     def backword(self,y):
+        t = time.time()
         # 一个梯度，对应上一层的数据，输入为128 * 10
-        sum = np.sum(np.exp(self.input_maps))
+        mini_batch,num_class = y.shape
+        sum = np.sum(np.exp(self.input_maps),axis=1).reshape((mini_batch,1))
         self.gradient = y * (sum-y)/sum**2
+        print ("softmax ",time.time()-t)
         # 输出为 128*10
 
 
@@ -244,25 +271,87 @@ class Model(object):
 x1 = np.arange(0,1,0.1)
 x2 = np.arange(0.9,-0.1,-0.1)
 assert x1.shape ==x2.shape
-image = np.concatenate([np.tile(x1,(1,3,10,1)),np.tile(x2,(1,3,10,1))])-0.5
-label=np.array([[1,0],[0,1]])
+image = np.concatenate([np.tile(x1,(5,3,10,1)),np.tile(x2,(5,3,10,1))])-0.5
+label=np.array([[1,0],[1,0],[1,0],[1,0],[1,0],[0,1],[0,1],[0,1],[0,1],[0,1]])
 
+# model = Model([
+#         Conv(1,8,3,1,1),
+#         activition(),
+#         MaxPooling(2,2),
+#         Conv(8,16,3,1,1),
+#         activition(),
+#         MaxPooling(2,2),
+#         Conv(16,32,3,1,1),
+#         MaxPooling(2, 2),
+#         activition(),
+#         Flattan(),
+#         FC(288,32),
+#         activition(),
+#         FC(32,10),
+#         activition(),
+#         softMax()
+#     ])
 model = Model([
-        Conv(3,8,3,1,1),
-        activition(),
-        MaxPooling(2,2),
-        Conv(8,16,3,1,1),
-        activition(),
-        MaxPooling(2,2),
         Flattan(),
-        FC(64,2),
+        FC(784,32),
+        activition(),
+        FC(32,10),
         activition(),
         softMax()
-    ])
-for i in range(20):
-    s = model.forword(image)
-    print("result",s)
-    model.backword(label)
+])
+# import  time
+# t = time.time()
+# img= np.random.randn(10,1,28,28)
+# r = model.forword(img)
+# print (r.shape)
+# print(time.time()-t)
+# t = time.time()
+# y = np.random.randn(10,10)
+# model.backword(y)
+# print(time.time()-t)
+
+# model.backword()
+import os
+import argparse
+import logging
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(28, padding=4),
+    transforms.ToTensor(),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_loader = DataLoader(
+    datasets.MNIST(root='.data/mnist', train=True, download=True, transform=transform_train), batch_size=10,
+    shuffle=True, num_workers=2, drop_last=True
+)
+
+test_loader = DataLoader(
+    datasets.MNIST(root='.data/mnist', train=False, download=True, transform=transform_test),
+    batch_size=10, shuffle=False, num_workers=2, drop_last=True
+)
+def one_hot(x, K):
+    return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
+
+
+for x,y in train_loader:
+    print(y)
+    x = x.numpy()
+    y = one_hot(np.array(y.numpy()),10)
+    s = model.forword(x)
+    print("result",np.argmax(s,axis=1))
+    model.backword(y)
+
+
 
 
 
