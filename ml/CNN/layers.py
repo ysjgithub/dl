986d7f7cac561eed9fbe71a393e18d876d05d812
杨邵军 <ccnuysj@qq.com>
2020-04-24 20:04:16 +0800

    wodiu wozhendiao

--- ml/CNN/layers.py
+++ ml/CNN/layers.py
@@ -6,94 +6,90 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 class Conv(object):
-    def __init__(self,input_channel,output_channel,kernel=3,stride=1,padding=0):
+    def __init__(self,input_channel,output_channel,kernel=3,stride=1,padding=1):
         self.input_channel=input_channel
         self.output_channel = output_channel
         self.kernel=kernel
         self.stride=stride
         self.padding=padding
         self.output_maps=[]
-        self.core = np.array([np.random.randn(input_channel,kernel,kernel)/np.sqrt(input_channel*kernel*kernel/2) for _ in range(output_channel)])
+        self.core = np.array([np.random.randn(input_channel,kernel,kernel) for _ in range(output_channel)])
         self.gradient =None
         self.input_maps = None
-        self.delta = None
     def forword(self,x):
         # 输入为 128 * 3 * 10 *10
         self.input_maps = x
         x = np.pad(x, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), 'constant')
-
-        self.delta = np.zeros(x.shape)
         mini_batch,c,w,h = x.shape
         neww,newh = int((w-self.kernel)/self.stride+1),int((h-self.kernel)/self.stride+1)
 
-        #图片
-        for img in x:
-            #卷积核
-            features = []
-            for core in self.core:
-                #卷积核对应一张特征图
-                featuremap = np.zeros((int(neww),int(newh)))
-                #遍历一张维度
-                for i in range(0,neww):
-                    for j in range(0,newh):
-                        featuremap[int(i/self.stride),int(j/self.stride)] = np.mean(img[:,i:i+self.kernel,j:j+self.kernel] * core)
-                features.append(featuremap)
-            self.output_maps.append(np.array(features))
-        self.output_maps = np.array(self.output_maps)
+        self.output_maps = np.zeros((mini_batch,self.output_channel,neww,newh))
+        for idc in range(self.output_channel):
+            core = self.core[idc,:,:,:]
+            #卷积核对应mini_batch张特征图
+            featuremap = np.zeros((mini_batch,neww,newh))
+
+            for i in range(neww):
+                for j in range(newh):
+                    newfeaturemap = np.sum(x[:,:,i:i+self.kernel,j:j+self.kernel] * core,axis=(1,2,3)).reshape((1,mini_batch))
+                    featuremap[:,i,j] = newfeaturemap
+
+        self.output_maps[:,idc,:,:] = featuremap
         #输出为128 * output_channel * 10 *10
 
     def update(self,y):
         # 更新权值要用到所有参与卷积的点，需要使用padding后的图像
         mini_batch,output_channel,w,h = y.shape
         x = np.pad(self.input_maps, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), 'constant')
+
         # 16 * 9 * 9
         for idc in range(self.output_channel):
             #更新一个核,core input_channel * 3 * 3,使用一组特征图
             core = self.core[idc,:,:,:]
             deltacore = np.zeros(core.shape)
 
-            for idi in range(mini_batch):
-                # 一个mini_batch的梯度加起来,feature 7 * 7
-                # 扩维
-                feature = np.expand_dims(y[idi,idc,:,:],axis=0)
-                feature = np.repeat(feature,self.input_channel,axis=0)
-                # 影响的对象只有一张图,features 7 * 7
+            # 一个mini_batch的梯度加起来
+            feature = y[:,idc,:,:].reshape((mini_batch,1,w,h))
+            assert feature.shape==(mini_batch,1,w,h)
+            feature = np.repeat(feature,self.input_channel,axis=1)
+            assert feature.shape==(mini_batch,self.input_channel,w,h)
 
-                for i in range(self.kernel):
-                    for j in range(self.kernel):
-                        zz= x[idi,:,i:i + w, j:j + h] * feature
-                        deltacore[:,i,j] += np.mean(np.mean(zz,axis=1),axis=1)
+            # 影响的对象只有一张图,features 7 * 7
 
-            deltacore=deltacore/len(y)
-            self.core[idc,:,:,:]-=deltacore*0.1
+            for i in range(self.kernel):
+                for j in range(self.kernel):
+                    zz= x[:,:,i:i + w, j:j + h] * feature
+                    deltacore[:,i,j] += np.sum(zz,axis=(0,2,3))/mini_batch
+            self.core[idc,:,:,:]-=deltacore*0.005
 
-    def backword(self,y):
+    def backword(self,m):
         # 输入为 output_maps，更新core的
-        # 输入为 128 * 10 * 10 *10
+        # 输入为 20 * 8 * 10 *10
         # 计算输入点产生的误差
-        t = time.time()
-
-        self.update(y)
-        mini_batch, c, w, h = y.shape
+        y=m
         mini_batch, ic, iw, ih = self.input_maps.shape
-
         y = np.pad(y, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), 'constant')
-
         self.gradient = np.zeros(self.input_maps.shape)
-        # 图片
-        for idx in range(mini_batch):
-            img = y[idx,:,:,:] #每一张图片的特征图 64 *7*7
-            # 遍历特征图，找到每个特征图产生误差的地方
-            for idf in range(len(img)):
-                cores = self.core[idf,:,:,:] # 32 * 3 * 3
-                featuremap = np.zeros((ic,iw,ih)) # 32 * 7 * 7
-                for idc in range(len(cores)):
-                    newcore = cores[idc,:,:].T[::-1].T[::-1] #3*3
-                    for i in range(0, w + 2 * self.padding - self.kernel, self.stride):
-                        for j in range(0, h + 2 * self.padding - self.kernel, self.stride):
-                            featuremap[idc,int(i / self.stride), int(j / self.stride)] = np.mean(img[:, i:i + self.kernel, j:j + self.kernel] * newcore)
-                    # 得到特征图加到误差点上
-                self.gradient[idx,:,:,:]+=featuremap
+        newcore = np.zeros((self.input_channel,self.output_channel,self.kernel,self.kernel))
+
+        for i in range(self.input_channel):
+            for j in range(self.output_channel):
+                newcore[i,j,:,:] = self.core[j,i,:,:].T[::-1].T[::-1]
+
+        for idc in range(self.input_channel):
+            core = newcore[idc,:,:,:]
+
+            # 卷积核对应mini_batch张特征图
+            featuremap = np.zeros((mini_batch, iw, ih))
+
+            for i in range(iw):
+                for j in range(ih):
+                    newfeaturemap = np.sum(y[:, :, i:i + self.kernel, j:j + self.kernel] * core,axis=(1, 2, 3)).reshape((1, mini_batch))
+                    featuremap[:, int(i / self.stride), int(j / self.stride)] = newfeaturemap
+
+            self.gradient[:, idc, :, :] = featuremap
+
+        self.update(m)
         self.output_maps =[]
         #输出大小为input_maps,去掉padding
 
@@ -170,7 +166,6 @@ class Flattan(object):
         for maps in x:
             self.output_maps.append(maps.flatten())
         self.output_maps = np.array(self.output_maps)
-        print (self.output_maps.shape)
         #输出是 128 * 250
     def backword(self,y):
         # t = time.time()
@@ -226,6 +221,7 @@ class softMax(object):
         self.output_maps = x/np.sum(x,axis=1,keepdims=True)
     def backword(self,y):
         self.gradient = y
+        # print("softmax",y)
 
 
 
@@ -255,19 +251,22 @@ def MSE(y,t):
     # 输出是 10 *10
 
 model = Model([
-        Conv(1,8,3,1,1),
+        Conv(1,32,3,1,1),
         activition(),
         MaxPooling(2,2),
-        Conv(8,16,3,1,1),
+        Conv(32,64,3,1,1),
         activition(),
         MaxPooling(2,2),
-        # Conv(16,32,3,1,1),
+        # Conv(64,128,3,1,1),
+        # activition(),
+        # MaxPooling(2, 2),
+        # Conv(128,256,3,1,1),
         # activition(),
         # MaxPooling(2, 2),
         Flattan(),
-        FC(784,32),
+        FC(3136,96),
         activition(),
-        FC(32,10),
+        FC(96,10),
         activition(),
         softMax()
     ])
@@ -295,14 +294,16 @@ test_loader = DataLoader(
 def one_hot(x, K):
     return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
 
-
-for i in range(20):
+iter = 0
+for i in range(2):
     for x,y in train_loader:
+        iter+=1
         x = x.numpy()
         y = one_hot(np.array(y.numpy()),10)
         s = model.forword(x)
         r = MSE(s,y)
-        print("result",np.argmax(y,axis=1),np.argmax(s,axis=1))
+        print(iter,np.argmax(y,axis=1),np.argmax(s,axis=1))
+        print(np.sum(np.argmax(y,axis=1)==np.argmax(s,axis=1)))
         model.backword(r)
 
 
