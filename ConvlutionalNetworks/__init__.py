846f953d8afa02fab5cb7ee0ae438d0e5f919668
杨邵军 <ccnuysj@qq.com>
2020-04-27 18:11:45 +0800

    0.0

--- ConvlutionalNetworks/__init__.py
+++
@@ -1,6 +0,0 @@
-from ConvlutionalNetworks import Conv as Conv
-from ConvlutionalNetworks import Flattan as Flattan
-from ConvlutionalNetworks import FullConnect as FC
-from ConvlutionalNetworks import MaxPooling as MaxPooling
-from ConvlutionalNetworks import Model as Model
-from ConvlutionalNetworks import Relu as Relu
